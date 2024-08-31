3dd164341125701b076a90ff3b3fbdbf70930de9
ZhiHua <zhcoder2011@gmail.com>
2024-09-01 03:26:01 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
-# rm -rf feeds/packages/net/mosdns
-# rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/ddns-go
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-ddns-go
 
@@ -30,61 +27,12 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-# git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-# git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
-# git_sparse_clone master https://github.com/syb999/openwrt-23.05.3 package/network/services/msd_lite
 git_sparse_clone openwrt-23.05 https://github.com/openwrt/packages net/tailscale
 
-# 添加rtl88x2bu驱动
-# git clone --branch v1.1.2 https://github.com/mirobiala/rtl88x2bu-cl package/kernel/rtl88x2bu-cl
-
-# 添加rtw88驱动
-# git clone https://github.com/henkv1/rtw88-usb-openwrt package/kernel/rtw88-usb
-
-# 添加rtw88-oot驱动
-# git_sparse_clone istoreos-22.03 https://github.com/istoreos/istoreos package/kernel/rtw88-oot
-
 # Themes
-git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
-
-# 修改Argon登录页footer信息呈现格式
-sed -i '
-/<footer>/,/<\/footer>/ c\
-<footer>\
-    <div style="text-align: center; word-wrap: break-word; overflow-wrap: break-word;">\
-        <div><a class="luci-link" href="https://github.com/openwrt/luci" target="_blank">Powered by <%= ver.luciname %><br>(<%= ver.luciversion %>)</a></div>\
-        <div><a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">ArgonTheme <%# vPKG_VERSION %></a></div>\
-        <div><%= ver.distversion %></div>\
-    </div>\
-</footer>
-' package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-
-# SmartDNS
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# DDNS-GO
-git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go  package/luci-app-ddns-go
-
-# msd_lite
-# git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
-# git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
-
-# MosDNS
-# git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-
-# iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
 
 # 取消主题默认设置
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
